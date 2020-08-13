b40cd9da63a1cfaf9d1e486473497262d0b54e6a
Carolain <Carolainsilva1@gmail.com>
2020-08-13 11:08:46 -0300

    funcion de prueba para recorrer un Array con indexOF

--- mateify/src/Components/Login/Funcionalidad/Functions.js
+++ mateify/src/Components/Login/Funcionalidad/Functions.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
-export const ValidacionUser = () => {
- return(
-     
- )
-}
+export function Functions () {
+    var myArray = [5, 10, 11, 2, 4];
+    if(myArray.indexOf(10) > -1){
+    var result =  <li>Item Exists in Array at index = {myArray.indexOf(99)} </li>
+   }else{
+    var result = <li>Item Not Found in Array.</li>
+   }
+     return (
+       <ul>{result}</ul>
+     );
+   }
